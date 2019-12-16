c24096b86b1a3c583068ff7f174f20cfe3de6d28
s-115-13 <s-115-13@gumiforest.ae.krakow.pl>
2019-12-16 14:39:52 +0100

    Zajęcia 08 (MyMatrix)

--- 08-DataStructures/MyMatrix.py
+++ 08-DataStructures/MyMatrix.py
@@ -2,8 +2,9 @@ class matrix():
 
     @staticmethod
     def create(x,y):
-        matrix = []
+        matrix = [[0 for w in range(y)] for z in range(x)] 
         value = 0
+        '''
         for i in range(x):
             # create single row
             row = []
@@ -11,12 +12,55 @@ class matrix():
                 row.append(value)
             # add row to matrix
             matrix.append(row)
+            '''
         return matrix
-
+    @staticmethod
+    def create_unit(x):
+        m=matrix.create(x,x)
+        pom=0
+        for v in range (0,len(m[0])):
+            m[pom][pom]=1
+            pom+=1
+        return m
+    @staticmethod
+    def fill_random(matrix, m, n):
+        import random
+        for v in matrix:
+            if isinstance(v, list):
+                licznik=-1
+                for p in v:
+                    licznik+=1
+                    pom=random.randint(m,n)
+                    v[licznik]=pom
+        return matrix            
+    @staticmethod
+    def transpose(matrix):
+        newmatrix=[]
+        licznik=-1
+        for value in matrix[0]:
+            newmatrix.append([])
+        wkolumny=len(matrix) #3
+        for v in range (0,len(matrix[0])):
+            licznik+=1
+            for w in range (0, wkolumny):
+                newmatrix[licznik].append(matrix[w][licznik])
+        return newmatrix
+                
     @staticmethod
     def print(matrix):
         for row in matrix:
             print(row)
 
-m = matrix.create(4,3)
+
+m = matrix.create(3,5)
+
+f=matrix.fill_random(m, 1, 9)
+#print(f)
 matrix.print(m)
+print()
+matrix.print(matrix.transpose(m))
+print()
+matrix.print(m)
+print()
+mm=matrix.create_unit(5)
+matrix.print(mm)
